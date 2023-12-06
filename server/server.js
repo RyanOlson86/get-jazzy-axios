25a3b292ddf3edfe058f5b44af0d3d509370579b
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-06 16:44:51 -0600

    added action to form

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 5001;
 
+// code for body-parser
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const artistListArray = [
     {
         name: 'Miles Davis',
@@ -47,6 +51,9 @@ const songListArray = [
 
 app.use(express.static('server/public'));
 
+// code for body-parser
+
+
 app.get('/artist', (req, res) => {
     res.send(artistListArray);
 });
@@ -60,4 +67,21 @@ app.get('/song', (req, res) => {
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
+});
+
+app.post('/', (req,res) => {
+    // The data (body) sent from the client is saved for us
+    // in `req.body`
+    // Note that without bodyParser setup, req.body will be undefined!
+    console.log(`Get a POST request!`, req.body);
+
+    // Grab the new quote from the request body
+    let quote = req.body.quoteToAdd;
+
+    // Push the quote into our array
+    console.log(`Adding new quote: `, quote)
+    quoteList.push(quote);
+
+    // Send back a status code of 201
+    res.sendStatus(201);
 });
